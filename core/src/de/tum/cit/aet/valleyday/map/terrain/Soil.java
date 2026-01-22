15599f098548159ff2e8eff3365b391552ef8895
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 16:40:42 +0100

    Jiafeng added the JavaDoc for Soil class.

--- core/src/de/tum/cit/aet/valleyday/map/terrain/Soil.java
+++ core/src/de/tum/cit/aet/valleyday/map/terrain/Soil.java
@@ -6,37 +6,62 @@ import de.tum.cit.aet.valleyday.map.TileObject;
 import static de.tum.cit.aet.valleyday.texture.Textures.GRASS;
 import static de.tum.cit.aet.valleyday.texture.Textures.NON_FARMLAND;
 
+/**
+ * Soil represents the ground type of a tile.
+ * It decides if crops can be planted or not.
+ */
 public class Soil extends TileObject {
 
     private SoilType type;
 
+    /**
+     * Create soil with a type.
+     */
     public Soil(int x, int y, SoilType type) {
         super(x, y);
         this.type = type;
     }
 
+    /**
+     * Change soil type.
+     */
     public void setType(SoilType type) {
         this.type = type;
     }
 
+    /**
+     * @return current soil type
+     */
     public SoilType getType() {
         return type;
     }
 
+    /**
+     * @return true if crops can be planted here
+     */
     public boolean isPlantable() {
         return type == SoilType.FARMLAND;
     }
 
+    /**
+     * Player can walk on soil.
+     */
     @Override
     public boolean isWalkable() {
         return true;
     }
 
+    /**
+     * Soil cannot be destroyed.
+     */
     @Override
     public boolean isDestructible() {
         return false;
     }
 
+    /**
+     * @return texture based on soil type
+     */
     @Override
     public TextureRegion getTexture() {
         return switch (type) {
@@ -45,3 +70,4 @@ public class Soil extends TileObject {
         };
     }
 }
+
