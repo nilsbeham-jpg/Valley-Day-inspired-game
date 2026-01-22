d3f97be37c833d0069af32951f76346d59de6241
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 17:49:54 +0100

    Jiafeng added the JavaDoc for PremiumCrop class

--- core/src/de/tum/cit/aet/valleyday/map/crops/PremiumCrop.java
+++ core/src/de/tum/cit/aet/valleyday/map/crops/PremiumCrop.java
@@ -3,25 +3,49 @@ package de.tum.cit.aet.valleyday.map.crops;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import static de.tum.cit.aet.valleyday.texture.Textures.*;
-import static de.tum.cit.aet.valleyday.texture.Textures.CROP_ROTTEN;
 
+/**
+ * PremiumCrop grows very slow but gives high value.
+ */
 public class PremiumCrop extends CropType {
 
     @Override
-    public float seedToSprout() { return 25f; }
+    public float seedToSprout() {
+        return 25f;
+    }
 
     @Override
-    public float sproutToMature() { return 25f; }
+    public float sproutToMature() {
+        return 25f;
+    }
 
     @Override
-    public float matureToRotten() { return 60f; }
+    public float matureToRotten() {
+        return 60f;
+    }
 
     @Override
-    public int harvestValue() { return 5; }
+    public int harvestValue() {
+        return 5;
+    }
 
+    @Override
+    public TextureRegion textureSeed() {
+        return CROP_SEED3;
+    }
 
-    @Override public TextureRegion textureSeed()   { return CROP_SEED3; }
-    @Override public TextureRegion textureSprout() { return CROP_SPROUT3; }
-    @Override public TextureRegion textureMature() { return CROP_MATURE3; }
-    @Override public TextureRegion textureRotten() { return CROP_ROTTEN3; }
+    @Override
+    public TextureRegion textureSprout() {
+        return CROP_SPROUT3;
+    }
+
+    @Override
+    public TextureRegion textureMature() {
+        return CROP_MATURE3;
+    }
+
+    @Override
+    public TextureRegion textureRotten() {
+        return CROP_ROTTEN3;
+    }
 }
