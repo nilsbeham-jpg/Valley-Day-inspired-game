cbe2730694327437bb0804f414173d488d6eeea7
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 16:17:04 +0100

    Jiafeng added the JavaDoc for the class RockDebris.

--- core/src/de/tum/cit/aet/valleyday/map/terrain/RockDebris.java
+++ core/src/de/tum/cit/aet/valleyday/map/terrain/RockDebris.java
@@ -3,14 +3,24 @@ package de.tum.cit.aet.valleyday.map.terrain;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.tum.cit.aet.valleyday.texture.Textures;
 
+/**
+ * RockDebris is a stronger type of debris.
+ * It uses a rock texture and needs shovel to remove.
+ */
 public class RockDebris extends Debris {
 
+    /**
+     * Create rock debris at a tile position.
+     */
     public RockDebris(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * @return rock texture
+     */
     @Override
     public TextureRegion getTexture() {
-        return Textures.ROCK; // add a rock texture
+        return Textures.ROCK;
     }
 }
