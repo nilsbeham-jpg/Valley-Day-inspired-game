59ee99e9d207a41291d065b4ed6d0d0d0b29d90a
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 15:10:29 +0100

    Jiafeng added the JavaDoc for the Debris class.

--- core/src/de/tum/cit/aet/valleyday/map/terrain/Debris.java
+++ core/src/de/tum/cit/aet/valleyday/map/terrain/Debris.java
@@ -4,21 +4,38 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.tum.cit.aet.valleyday.map.TileObject;
 import de.tum.cit.aet.valleyday.texture.Textures;
 
+/**
+ * Debris is an obstacle on the map.
+ * Player cannot walk through it, but can destroy it.
+ */
 public class Debris extends TileObject {
+
+    /**
+     * Create debris at a tile position.
+     */
     public Debris(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Debris blocks movement.
+     */
     @Override
     public boolean isWalkable() {
         return false;
     }
 
+    /**
+     * Debris can be destroyed by player.
+     */
     @Override
     public boolean isDestructible() {
         return true;
     }
 
+    /**
+     * @return debris texture
+     */
     @Override
     public TextureRegion getTexture() {
         return Textures.DEBRIS;
