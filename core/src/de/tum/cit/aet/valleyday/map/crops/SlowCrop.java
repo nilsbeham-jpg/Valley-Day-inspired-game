d0386ac4655bcc700f5e1641c4e864f779331b00
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 17:50:53 +0100

    Jiafeng added more JaveDoc to the another class.

--- core/src/de/tum/cit/aet/valleyday/map/crops/SlowCrop.java
+++ core/src/de/tum/cit/aet/valleyday/map/crops/SlowCrop.java
@@ -3,25 +3,50 @@ package de.tum.cit.aet.valleyday.map.crops;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import static de.tum.cit.aet.valleyday.texture.Textures.*;
-import static de.tum.cit.aet.valleyday.texture.Textures.CROP_ROTTEN;
 
+/**
+ * SlowCrop grows slower than basic crop.
+ * It gives medium harvest value.
+ */
 public class SlowCrop extends CropType {
 
     @Override
-    public float seedToSprout() { return 15f; }
+    public float seedToSprout() {
+        return 15f;
+    }
 
     @Override
-    public float sproutToMature() { return 15f; }
+    public float sproutToMature() {
+        return 15f;
+    }
 
     @Override
-    public float matureToRotten() { return 60f; }
+    public float matureToRotten() {
+        return 60f;
+    }
 
     @Override
-    public int harvestValue() { return 3; }
+    public int harvestValue() {
+        return 3;
+    }
 
+    @Override
+    public TextureRegion textureSeed() {
+        return CROP_SEED2;
+    }
 
-    @Override public TextureRegion textureSeed()   { return CROP_SEED2; }
-    @Override public TextureRegion textureSprout() { return CROP_SPROUT2; }
-    @Override public TextureRegion textureMature() { return CROP_MATURE2; }
-    @Override public TextureRegion textureRotten() { return CROP_ROTTEN2; }
+    @Override
+    public TextureRegion textureSprout() {
+        return CROP_SPROUT2;
+    }
+
+    @Override
+    public TextureRegion textureMature() {
+        return CROP_MATURE2;
+    }
+
+    @Override
+    public TextureRegion textureRotten() {
+        return CROP_ROTTEN2;
+    }
 }
