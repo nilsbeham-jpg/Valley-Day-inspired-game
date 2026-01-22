a2cce2b22d099bc232885658597986a557669f7f
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 16:13:31 +0100

    Jiafeng added the JavaDoc for  the Fence class.

--- core/src/de/tum/cit/aet/valleyday/map/terrain/Fence.java
+++ core/src/de/tum/cit/aet/valleyday/map/terrain/Fence.java
@@ -4,16 +4,30 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.tum.cit.aet.valleyday.map.TileObject;
 import de.tum.cit.aet.valleyday.texture.Textures;
 
+/**
+ * Fence is a solid object.
+ * Player cannot walk through it and cannot destroy it.
+ */
 public class Fence extends TileObject {
+
+    /**
+     * Create fence at a tile position.
+     */
     public Fence(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Fence blocks movement.
+     */
     @Override
     public boolean isWalkable() {
         return false;
     }
 
+    /**
+     * @return fence texture
+     */
     @Override
     public TextureRegion getTexture() {
         return Textures.FENCE;
