f0b979d06ff1b459fe6ddcc231735abc1904597a
Jiafeng Qian <jiafeng.qian@tum.de>
2026-01-22 14:45:28 +0100

    Jiafeng added the JavaDoc for the class Shovel.

--- core/src/de/tum/cit/aet/valleyday/map/Items/Shovel.java
+++ core/src/de/tum/cit/aet/valleyday/map/Items/Shovel.java
@@ -4,17 +4,30 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.tum.cit.aet.valleyday.map.GameMap;
 import de.tum.cit.aet.valleyday.texture.Textures;
 
+/**
+ * Shovel unlocks special actions.
+ * Player needs it to remove hard debris like tree
+ */
 public class Shovel extends Item {
 
+    /**
+     * Create shovel item.
+     */
     public Shovel(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * @return shovel texture
+     */
     @Override
     public TextureRegion getTexture() {
         return Textures.SHOVEL;
     }
 
+    /**
+     * Enable shovel ability on player.
+     */
     @Override
     public void onPickup(GameMap map) {
         map.getPlayer().enableShovel();
